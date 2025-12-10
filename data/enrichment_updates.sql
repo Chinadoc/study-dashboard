7413359c62ce44fa48b6abf4fc2e09617fd049fe
Chinadoc <jeremy.samuels17@gmail.com>
2025-12-09 20:50:51 -0600

    feat: D1 data enrichment - 27 curated FCC entries, 62k keyway updates
    - Added enrich_d1.js script to generate SQL from CSVs
    - Created curated_overrides table with 27 top FCC entries
      - Chrysler/Dodge/Jeep Fobik keys
      - Toyota H-chip keys
      - Honda, Ford, GM, Hyundai, Nissan, VW, BMW, Mercedes
    - Updated 62,284 rows with keyway data from Lishi mappings
    - All major makes now have keyway field populated

---
+++ data/enrichment_updates.sql
@@ -0,0 +1,45 @@
+-- D1 Enrichment Updates
+-- Generated: 2025-12-10T02:48:14.558Z
+-- Updates NULL fields only
+
+UPDATE locksmith_data SET keyway = 'HU66' WHERE keyway IS NULL AND LOWER(make) = 'volkswagen';
+UPDATE locksmith_data SET keyway = 'HU66' WHERE keyway IS NULL AND LOWER(make) = 'audi';
+UPDATE locksmith_data SET keyway = 'HU66' WHERE keyway IS NULL AND LOWER(make) = 'seat';
+UPDATE locksmith_data SET keyway = 'HU66' WHERE keyway IS NULL AND LOWER(make) = 'skoda';
+UPDATE locksmith_data SET keyway = 'HU92' WHERE keyway IS NULL AND LOWER(make) = 'bmw';
+UPDATE locksmith_data SET keyway = 'HU100' WHERE keyway IS NULL AND LOWER(make) = 'vauxhall';
+UPDATE locksmith_data SET keyway = 'HU100' WHERE keyway IS NULL AND LOWER(make) = 'opel';
+UPDATE locksmith_data SET keyway = 'HU100' WHERE keyway IS NULL AND LOWER(make) = 'buick';
+UPDATE locksmith_data SET keyway = 'HU100' WHERE keyway IS NULL AND LOWER(make) = 'chevrolet';
+UPDATE locksmith_data SET keyway = 'HU100' WHERE keyway IS NULL AND LOWER(make) = 'holden';
+UPDATE locksmith_data SET keyway = 'HU101' WHERE keyway IS NULL AND LOWER(make) = 'ford';
+UPDATE locksmith_data SET keyway = 'HU101' WHERE keyway IS NULL AND LOWER(make) = 'jaguar';
+UPDATE locksmith_data SET keyway = 'HU101' WHERE keyway IS NULL AND LOWER(make) = 'land rover';
+UPDATE locksmith_data SET keyway = 'HU101' WHERE keyway IS NULL AND LOWER(make) = 'volvo';
+UPDATE locksmith_data SET keyway = 'HON66' WHERE keyway IS NULL AND LOWER(make) = 'honda';
+UPDATE locksmith_data SET keyway = 'HON66' WHERE keyway IS NULL AND LOWER(make) = 'acura';
+UPDATE locksmith_data SET keyway = 'TOY43' WHERE keyway IS NULL AND LOWER(make) = 'toyota';
+UPDATE locksmith_data SET keyway = 'TOY43' WHERE keyway IS NULL AND LOWER(make) = 'lexus';
+UPDATE locksmith_data SET keyway = 'TOY43' WHERE keyway IS NULL AND LOWER(make) = 'pontiac';
+UPDATE locksmith_data SET keyway = 'TOY48' WHERE keyway IS NULL AND LOWER(make) = 'scion';
+UPDATE locksmith_data SET keyway = 'CY24' WHERE keyway IS NULL AND LOWER(make) = 'chrysler';
+UPDATE locksmith_data SET keyway = 'CY24' WHERE keyway IS NULL AND LOWER(make) = 'dodge';
+UPDATE locksmith_data SET keyway = 'CY24' WHERE keyway IS NULL AND LOWER(make) = 'jeep';
+UPDATE locksmith_data SET keyway = 'MAZ24R' WHERE keyway IS NULL AND LOWER(make) = 'mazda';
+UPDATE locksmith_data SET keyway = 'MIT11R' WHERE keyway IS NULL AND LOWER(make) = 'mitsubishi';
+UPDATE locksmith_data SET keyway = 'NE72' WHERE keyway IS NULL AND LOWER(make) = 'peugeot';
+UPDATE locksmith_data SET keyway = 'NE72' WHERE keyway IS NULL AND LOWER(make) = 'citroen';
+UPDATE locksmith_data SET keyway = 'NSN14' WHERE keyway IS NULL AND LOWER(make) = 'nissan';
+UPDATE locksmith_data SET keyway = 'NSN14' WHERE keyway IS NULL AND LOWER(make) = 'infiniti';
+UPDATE locksmith_data SET keyway = 'HY16' WHERE keyway IS NULL AND LOWER(make) = 'hyundai';
+UPDATE locksmith_data SET keyway = 'HY16' WHERE keyway IS NULL AND LOWER(make) = 'kia';
+UPDATE locksmith_data SET keyway = 'VA6' WHERE keyway IS NULL AND LOWER(make) = 'renault';
+UPDATE locksmith_data SET keyway = 'VA6' WHERE keyway IS NULL AND LOWER(make) = 'dacia';
+UPDATE locksmith_data SET keyway = 'SIP22' WHERE keyway IS NULL AND LOWER(make) = 'fiat';
+UPDATE locksmith_data SET keyway = 'SIP22' WHERE keyway IS NULL AND LOWER(make) = 'alfa romeo';
+UPDATE locksmith_data SET keyway = 'SIP22' WHERE keyway IS NULL AND LOWER(make) = 'lancia';
+UPDATE locksmith_data SET keyway = 'B106' WHERE keyway IS NULL AND LOWER(make) = 'general motors';
+UPDATE locksmith_data SET keyway = 'YM23' WHERE keyway IS NULL AND LOWER(make) = 'mercedes-benz';
+UPDATE locksmith_data SET keyway = 'KW14' WHERE keyway IS NULL AND LOWER(make) = 'kawasaki';
+UPDATE locksmith_data SET keyway = 'SU14' WHERE keyway IS NULL AND LOWER(make) = 'suzuki';
+UPDATE locksmith_data SET keyway = 'YH35R' WHERE keyway IS NULL AND LOWER(make) = 'yamaha';
